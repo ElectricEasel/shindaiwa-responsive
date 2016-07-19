72fd8294bc1822d9ba9f83a12e634d823b4c6814
Ricky Goldman <ricky@electriceasel.com>
2016-07-19 09:12:56 -0500

    you_can resources

---
+++ you_can.php
@@ -0,0 +1,100 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<?php include 'head.php' ?>
+</head>
+
+<body class="site com_content view-featured no-layout no-task itemid-101 page-home">
+<div id="main-wrap" class="collapsed">
+
+	<?php include 'header.php' ?>
+	<div class="body">
+
+		<div class="banner-above day7-banner">
+			<div class="content-block">
+				<div class="you_can-banner__text text-uppercase">
+					<div class="you_can-banner__title">7-DAY</div>
+					<div class="you_can-banner__subtitle">money back <span>guarantee!</span></div>
+				</div>
+				<div class="you_can-banner__icon">
+					<img src="/images/temp/day7-icon.png" title="" alt="">
+				</div>
+				<div class="you_can-banner__instrument">
+					<img src="/images/temp/you_can-banner-instrument.png" title="" alt="">
+				</div>
+			</div>
+
+		</div>
+
+		<div id="content-container">
+			<main id="content">
+
+				<div class="container-fluid day7-content">
+					<div class="container">
+						<div class="row">
+							<div class="day7-content__title text-center text-uppercase">
+								Introducing the Shindaiwa 7-Day Money Back Guarantee!
+							</div>
+							<div class="day7-content__guarantee-block text-center clearfix">
+								<div class="day7-content__guar-text ">
+									We’re so positive you’ll be happy with your Shindaiwa purchase, we
+									<span>Guarantee it!</span>
+								</div>
+								<div class="day7-content__guar-img">
+									<img src="/images/temp/day7-guarantee-img.png" alt="" title="">
+								</div>
+							</div>
+							<div class="day7-content__text">
+								<p>
+									But if, for some reason, you’re not satisfied with the exceptional performance of a
+									Shindaiwa unit, simply bring it back to the dealer within seven days. No questions
+									asked! This program is effective on all purchases after January 1, 2013. Visit your
+									local Shindaiwa dealer for more details.
+								</p>
+								<p>
+									This program includes all engine-driven units and Multi-Tool Accessories only. No
+									other parts or accessories are considered part of this program.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div class="container-fluid day7-download">
+					<div class="container">
+						<div class="row">
+							<div class="col-sm-4">
+								<div class="day7-download__img text-center">
+									<img src="/images/temp/warranty-banner-img.png" title="" alt="">
+								</div>
+							</div>
+							<div class="col-sm-8">
+								<div class="title-hBlack-red day7-download__title">
+									5-Year Consumer & <br>2-Year Commercial Warranties
+								</div>
+							</div>
+						</div>
+						<div class="row">
+							<div class="day7-download__text">
+								<p>
+									Shindaiwa manufactures premium, high quality products, and to support our
+									confidence in the product, we offer a 5-year consumer warranty and two-year
+									commercial warranty. Shindaiwa offers two of the strongest warranties in the
+									industry!
+								</p>
+							</div>
+							<div class="download-pdf">
+								<a href="#" class="text-uppercase">Shindaiwa 2013 Limited Warranty</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</main>
+		</div>
+
+	</div>
+	<?php include 'footer.php' ?>
+</div>
+</body>
+</html>
