1c06c39e8e26d1696f9f648474009e38f0de8244
Ricky Goldman <ricky@electriceasel.com>
2016-07-19 12:42:28 -0500

    you can page

--- you_can.php
+++ you_can.php
@@ -11,90 +11,282 @@
 	<?php include 'header.php' ?>
 	<div class="body">
 
-		<div class="banner-above day7-banner">
+		<div class="banner-above you_can-banner">
 			<div class="content-block">
-				<div class="you_can-banner__text text-uppercase">
-					<div class="you_can-banner__title">7-DAY</div>
-					<div class="you_can-banner__subtitle">money back <span>guarantee!</span></div>
+				<div class="you_can-banner__text">
+					<div class="you_can-banner__title text-uppercase">You Can Do it! <span>It&rsquo;s Easy</span></div>
+					<div class="you_can-banner__subtitle"><span>Repair can be costly...</span> Maintenance Saves You Time &amp; Money!</div>
 				</div>
-				<div class="you_can-banner__icon">
-					<img src="/images/temp/day7-icon.png" title="" alt="">
-				</div>
-				<div class="you_can-banner__instrument">
-					<img src="/images/temp/you_can-banner-instrument.png" title="" alt="">
+
+				<div class="you_can-banner__badge">
+					<img class="hide-small" src="images/temp/you_can_maintenance_kit.png" alt="Shindaiwa&reg; You Can Maintenance Kit">
+					<img class="hide-desktop" src="images/temp/you_can_maintenance_kit_small.png" alt="Shindaiwa&reg; You Can Maintenance Kit">
 				</div>
 			</div>
-
 		</div>
+		<div class="maintenance-banner">
+            <a href="#" class="bg-red">
+                <h5>Find Your Maintenance Kit <span>Go!</span></h5>
+            </a>
+        </div>
 
 		<div id="content-container">
 			<main id="content">
 
-				<div class="container-fluid day7-content">
+				<div class="container-fluid you_can-content">
 					<div class="container">
 						<div class="row">
-							<div class="day7-content__title text-center text-uppercase">
-								Introducing the Shindaiwa 7-Day Money Back Guarantee!
-							</div>
-							<div class="day7-content__guarantee-block text-center clearfix">
-								<div class="day7-content__guar-text ">
-									We’re so positive you’ll be happy with your Shindaiwa purchase, we
-									<span>Guarantee it!</span>
-								</div>
-								<div class="day7-content__guar-img">
-									<img src="/images/temp/day7-guarantee-img.png" alt="" title="">
-								</div>
-							</div>
-							<div class="day7-content__text">
-								<p>
-									But if, for some reason, you’re not satisfied with the exceptional performance of a
-									Shindaiwa unit, simply bring it back to the dealer within seven days. No questions
-									asked! This program is effective on all purchases after January 1, 2013. Visit your
-									local Shindaiwa dealer for more details.
-								</p>
-								<p>
-									This program includes all engine-driven units and Multi-Tool Accessories only. No
-									other parts or accessories are considered part of this program.
-								</p>
-							</div>
-						</div>
+							<div id="you-can-details">
+			                    <h3>A Complete Offering of Maintenance Kits:</h3>
+			                    <div class="row">
+			                        <ul class="col-sm-6">
+			                            <li><strong>Extend the life</strong> of your equipment.</li>
+			                            <li><strong>Reduce costly repairs</strong> and downtime with routine maintenance.</li>
+			                            <li><strong>Save money</strong> doing the installation yourself. You can do it; it's easy!</li>
+			                        </ul>
+			                        <ul class="col-sm-6">
+			                            <li><strong>Save time</strong> get everything you need in one convenient package.</li>
+			                            <li><strong>Genuine Shindaiwa replacement parts</strong> deliver high-performance results.</li>
+			                        </ul>
+			                    </div>
+			                </div>
+
+			                <div id="spot-marketing">
+			                    <div class="slideshow-wrap">
+			                    <div id="you-can-carousel" class="owl-carousel">
+			                        <div class="spot-item">
+			                            <h4>Tune-Up Kits</h4>
+			                            <div class="img-wrap"><img src="images/temp/yc_tune_up.png" alt="" /></div>
+			                            <p><strong>Tune-up Kits</strong> include spark plugs, fuel filter &amp; air filter. Lorem ipsum dolor sit amet tu lorem ipsum.</p>
+			                        </div>
+			                        <div class="spot-item">
+			                            <h4>Fuel System Kits</h4>
+			                            <div class="img-wrap"><img src="images/temp/yc_fuel_system.png" alt="" /></div>
+			                            <p><strong>Fuel System Kits</strong> include spark plugs, fuel filter &amp; ait filter. Lorem ipsum dolor sit amet tu lorem ipsum.</p>
+			                        </div>
+			                        <div class="spot-item">
+			                            <h4>Gas Caps</h4>
+			                            <div class="img-wrap"><img src="images/temp/yc_gas_caps.png" alt="" /></div>
+			                            <p><strong>Gas Caps</strong> are ipsum dolor sit amet tu lorem ipsum. Lorem ipsum dolor sit amet tu lorem ipsum.</p>
+			                        </div>
+			                        <div class="spot-item">
+			                            <h4>Tune-Up Kits</h4>
+			                            <div class="img-wrap"><img src="images/temp/yc_tune_up.png" alt="" /></div>
+			                            <p><strong>Tune-up Kits</strong> include spark plugs, fuel filter &amp; air filter. Lorem ipsum dolor sit amet tu lorem ipsum.</p>
+			                        </div>
+			                        <div class="spot-item">
+			                            <h4>Fuel System Kits</h4>
+			                            <div class="img-wrap"><img src="images/temp/yc_fuel_system.png" alt="" /></div>
+			                            <p><strong>Fuel System Kits</strong> include spark plugs, fuel filter &amp; ait filter. Lorem ipsum dolor sit amet tu lorem ipsum.</p>
+			                        </div>
+			                        <div class="spot-item">
+			                            <h4>Gas Caps</h4>
+			                            <div class="img-wrap"><img src="images/temp/yc_gas_caps.png" alt="" /></div>
+			                            <p><strong>Gas Caps</strong> are ipsum dolor sit amet tu lorem ipsum. Lorem ipsum dolor sit amet tu lorem ipsum.</p>
+			                        </div>
+			                    </div>
+			                    <a href="javascript:void(0)" id="prev-spot" class="spot-control left"><img src="/images/arrl.png" alt="&lt;" /></a>
+			                    <a href="javascript:void(0)" id="next-spot" class="spot-control right"><img src="/images/arrr.png" alt="&gt;" /></a>
+			                    </div>
+			                </div>
+			            </div>
 					</div>
 				</div>
 
-				<div class="container-fluid day7-download">
-					<div class="container">
-						<div class="row">
-							<div class="col-sm-4">
-								<div class="day7-download__img text-center">
-									<img src="/images/temp/warranty-banner-img.png" title="" alt="">
-								</div>
-							</div>
-							<div class="col-sm-8">
-								<div class="title-hBlack-red day7-download__title">
-									5-Year Consumer & <br>2-Year Commercial Warranties
-								</div>
-							</div>
-						</div>
-						<div class="row">
-							<div class="day7-download__text">
-								<p>
-									Shindaiwa manufactures premium, high quality products, and to support our
-									confidence in the product, we offer a 5-year consumer warranty and two-year
-									commercial warranty. Shindaiwa offers two of the strongest warranties in the
-									industry!
-								</p>
-							</div>
-							<div class="download-pdf">
-								<a href="#" class="text-uppercase">Shindaiwa 2013 Limited Warranty</a>
-							</div>
-						</div>
-					</div>
-				</div>
+				<div class="you-can-sub-content maintenance-schedule">
+		            <div class="container">
+		            <div class="table-wrap">
+		                <table class="hide-small">
+		                <thead>
+		                    <tr class="main-heading">
+		                        <th colspan="5">Maintenance Schedule</th>
+		                    </tr>
+		                    <tr>
+		                        <th>Component</th>
+		                        <th>Procedure</th>
+		                        <th>Before Each Use</th>
+		                        <th>3 Months or 90 Hours</th>
+		                        <th>Yearly</th>
+		                    </tr>
+		                </thead>
+		                <tbody>
+		                    <tr class="gray">
+		                        <td class="caps">Spark Plug</td>
+		                        <td>Replace</td>
+		                        <td></td>
+		                        <td><i>x</i></td>
+		                        <td></td>
+		                    </tr>
+		                    <tr>
+		                        <td class="caps" rowspan="2">Air Filter</td>
+		                        <td>Clean</td>
+		                        <td><i>x</i></td>
+		                        <td></td>
+		                        <td></td>
+		                    </tr>
+		                    <tr>
+		                        <td>Replace</td>
+		                        <td></td>
+		                        <td><i>x</i></td>
+		                        <td><i>x</i></td>
+		                    </tr>
+		                    <tr class="gray">
+		                        <td class="caps">Fuel Filter</td>
+		                        <td>Replace</td>
+		                        <td></td>
+		                        <td><i>x</i></td>
+		                        <td></td>
+		                    </tr>
+		                    <tr>
+		                        <td class="caps">Fuel Lines</td>
+		                        <td>Replace</td>
+		                        <td></td>
+		                        <td><i>x</i></td>
+		                        <td></td>
+		                    </tr>
+		                    <tr class="gray">
+		                        <td class="caps">Guide Bar Nuts</td>
+		                        <td>Inspect</td>
+		                        <td><i>x</i></td>
+		                        <td></td>
+		                        <td></td>
+		                    </tr>
+		                </tbody>
+		                </table>
+		                <table class="hide-desktop">
+		                <thead>
+		                    <tr class="main-heading">
+		                        <th colspan="5">Maintenance Schedule</th>
+		                    </tr>
+		                </thead>
+		                <tbody>
+		                    <tr class="black">
+		                        <th colspan="2">Spark Plug</th>
+		                    </tr>
+		                    <tr>
+		                        <td>Replace</td>
+		                        <td>3 months or 90 hours</td>
+		                    </tr>
+		                    <tr class="black">
+		                        <th colspan="2">Air Filter</th>
+		                    </tr>
+		                    <tr>
+		                        <td>Clean</td>
+		                        <td>Before Each Use</td>
+		                    </tr>
+		                    <tr>
+		                        <td rowspan="2">Replace</td>
+		                        <td>3 months or 90 hours</td>
+		                    </tr>
+		                    <tr>
+		                        <td>Yearly</td>
+		                    </tr>
+		                    <tr class="black">
+		                        <th colspan="2">Fuel Filter</th>
+		                    </tr>
+		                    <tr>
+		                        <td>Replace</td>
+		                        <td>3 months or 90 hours</td>
+		                    </tr>
+		                    <tr class="black">
+		                        <th colspan="2">Fuel Lines</th>
+		                    </tr>
+		                    <tr>
+		                        <td>Replace</td>
+		                        <td>3 months or 90 hours</td>
+		                    </tr>
+		                    <tr class="black">
+		                        <th colspan="2">Guide Bar Nuts</th>
+		                    </tr>
+		                    <tr>
+		                        <td>Inspect</td>
+		                        <td>Before Each Use</td>
+		                    </tr>
+		                </tbody>
+		                </table>
+		                <div class="white-corner top left"></div>
+		                <div class="white-corner top right"></div>
+		                <div class="white-corner bottom left"></div>
+		                <div class="white-corner bottom right"></div>
+		            </div>
+		                <p><em><span>Maintenance intervals are recommendations. </span>Levels of use, conditions and experience will also impact frequency of replacement.</em></p>
+		                <a class="btn btn-red" href="#">Download Spec Sheet</a>
+		            </div>
+		        </div>
+
+		        <div class="gray-bg you-can-sub-content gradient">
+		            <div class="container">
+		                <div id="peak-condition">
+		                    <h3 class="black-bordered">Keep your equipment running in peak condition!</h3>
+		                    <h6>Look for these and other great products</h6>
+		                    <div class="row redarmor">
+		                        <div class="col-sm-12">
+		                            <img class="ra-logo" src="images/temp/logo-red-armor-oil.png" alt="Shindaiwa Red Armor Oil">
+		                            <img class="ra-product margin-top" src="images/temp/bottle-red-armor-oil.png" alt="Shindaiwa Red Armor Oil">
+		                            <div class="product-desc">
+		                                <h5>2-Cycle Engine Oil</h5>
+		                                <ul>
+		                                    <li>Red Armor formula delivers the industry&rsquo;s best in engine cleaning protection</li>
+		                                </ul>
+		                                <a href="#">Learn More</a>
+		                            </div>
+		                        </div>
+		                    </div>
+		                </div>
+		            </div>
+		        </div>
+
+		        <div class="maintenance-banner">
+		            <a href="#" class="bg-red">
+		                <h5>Find Your Maintenance Kit <span>Go!</span></h5>
+		            </a>
+		        </div>
+
+		        <div id="additional-support">
+		            <div class="container">
+		                <img class="wrench" src="images/temp/icon_wrench.png" alt="">
+		                <h4>Additional Support/Help</h4>
+		                <p><strong>For assistance or questions concerning your Shindaiwa product:</strong></p>
+		                <p>Call Us Toll Free: 877.9.TORQUE (Monday-Friday - 8:30am-4:30pm EST)</p>
+		            </div>
+		        </div>
+
 			</main>
 		</div>
 
 	</div>
 	<?php include 'footer.php' ?>
+    <script type="text/javascript">
+        $(document).ready(function(){
+            var youCanCarousel = $('#you-can-carousel');
+            youCanCarousel.owlCarousel({
+                loop: true,
+                dots: true,
+                autoplay: false,
+                responsive: {
+                    0: {
+                        items:1,
+                        margin:0
+                    },
+                    480: {
+                        items:2,
+                        margin:25
+                    },
+                    992: {
+                        items:3,
+                        margin:25
+                    }
+                }
+            });
+
+            $('#prev-spot').click(function(){
+                youCanCarousel.trigger('prev.owl.carousel');
+            });
+            $('#next-spot').click(function(){
+                youCanCarousel.trigger('next.owl.carousel');
+            });
+        });
+    </script>
 </div>
 </body>
 </html>
