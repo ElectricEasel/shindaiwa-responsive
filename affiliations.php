b62651f157e28cc3634c6af8dfa2b9bab8b63fbc
Ricky Goldman <ricky@electriceasel.com>
2016-08-09 14:06:24 -0500

    affiliations page

---
+++ affiliations.php
@@ -0,0 +1,121 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<?php include 'head.php' ?>
+	<script type="text/javascript" src="js/template2.js"></script>
+</head>
+
+<body class="site com_content view-featured no-layout no-task itemid-101 page-home">
+<div id="main-wrap" class="collapsed">
+
+	<?php include 'header.php' ?>
+	<div class="body">
+		<div class="banner-inner" >
+			<div class="container banner-inner__text text-left text-uppercase">
+				<h1 class="banner-inner__title">about shindaiwa</h1>
+			</div>
+		</div>
+
+		<div class="container-fluid breadcrumb-wrap hidden-xs clearfix">
+			<div class="container">
+				<ul class="breadcrumb">
+					<li>
+						<a href="">Home</a>
+					</li>
+					<li>
+						<a href="">About Shindaiwa</a>
+					</li>
+
+					<li>
+						Affiliations
+					</li>
+				</ul>
+			</div>
+		</div>
+
+		<div id="content-container">
+			<main id="content">
+
+				<div class=" about-us clearfix">
+					<div class="container">
+						<div class="row">
+							<div class="col-md-3 hidden-sm hidden-xs">
+								<div class="about-aside">
+									<h3 class="about-aside__tit">about shindaiwa</h3>
+									<ul class="about-aside__list">
+										<li><a href="">About Us</a></li>
+										<li><a href="">News & Events</a></li>
+										<li><a href="">Testimonials</a></li>
+										<li><a href="">Press Releases</a></li>
+										<li>
+											<a href="">Contact Us</a>
+											<ul class="about-aside__sublist">
+												<li class="active"><a href="">Newsletter Signup</a></li>
+											</ul>
+										</li>
+										<li><a href="">Video Library</a></li>
+										<li><a href="">Distributor Network</a></li>
+										<li class="active"><a href="">Affiliations</a></li>
+									</ul>
+								</div>
+							</div>
+							<div class="contact col-md-9">
+								<h2 class="contact__title about">
+									Affiliations
+								</h2>
+								<div class="afilliations-list">
+									<div class="affiliation nhla">
+										<div class="logo"><img src="images/temp/affil-nhla.png" alt="NHLA" /></div>
+										<h4><a href="">National Hispanic Landscape Alliance</a></h4>
+										<p>More than 500,000 Hispanic-Americans work in the landscape industry across the country. NHLA was formed in an effort to protect and advance the interests of these professionals. The organization works to inform, encourage, and support their members in order to help them become successful professionals, engaged leaders, and effective advocates.</p>
+									</div>
+									<div class="affiliation nalp">
+										<div class="logo"><img src="images/temp/affil-nalp.png" alt="NALP" /></div>
+										<h4><a href="">National Association of Landscape Professionals</a></h4>
+										<p>NALP represents the voice of more than 100,000 landscape and lawn care professionals, including industry companies, students, consultants, suppliers, state associations, and other affiliates. NALP is committed to helping their members succeed and advocating for the economic, social, and environmental benefits of the landscape industry.</p>
+									</div>
+									<div class="affiliation opei">
+										<div class="logo"><img src="images/temp/affil-opei.png" alt="OPEI" /></div>
+										<h4><a href="">Outdoor Power Equipment Institute</a></h4>
+										<p>OPEI is an international trade association that represents more than 100 power equipment, engine, and utility vehicle manufacturers and suppliers. The association is a recognized developer of safety and performance standards within the industry. OPEI is also the creative force behind the family environmental education program, TurfMutt.</p>
+									</div>
+									<div class="affiliation pgms">
+										<div class="logo"><img src="images/temp/affil-pgms.png" alt="PGMS" /></div>
+										<h4><a href="">Professional Grounds Management Society</a></h4>
+										<p>PGMS was established as a professional society for individual ground managers who work for organizations like colleges and universities, municipalities, park and recreation facilities, office parks, apartment complexes, hotels and motels, cemeteries, and landscape management companies. The society works to further the grounds management profession through education, professional development, and economic advancement.</p>
+									</div>
+									<div class="affiliation tcia">
+										<div class="logo"><img src="images/temp/affil-tcia.png" alt="TCIA" /></div>
+										<h4><a href="">Tree Care Industry Association</a></h4>
+										<p>Known as “The Voice of Tree Care,” TCIA’s mission is to advance tree care businesses through the development and promotion of educational programs, standards of tree care, and management for arboriculture firms around the world. TCIA also helps consumers identify qualified tree care companies with their Accreditation program.</p>
+									</div>
+									<div class="affiliation acf">
+										<div class="logo"><img src="images/temp/affil-acf.png" alt="ACF" /></div>
+										<h4><a href="">Association of Consulting Foresters</a></h4>
+										<p>ACT is comprised of independent forestry professionals across the country who manage forests and market forest products for private landowners. The association allows these professionals to share their experience and expertise with each other, and disseminate new knowledge and skills in the industry. The designation of ACF is a way for landowners to identify consulting foresters with proven ability and character.</p>
+									</div>
+									<div class="affiliation isa">
+										<div class="logo"><img src="images/temp/affil-isa.png" alt="ISA" /></div>
+										<h4><a href="">International Society of Arboriculture</a></h4>
+										<p>ISA’s main mission is to promote the professional practice of arboriculture — the cultivation, management, and study of trees — through research, technology, and education. They work actively to foster a greater awareness of the benefits of trees and tree care among professional arborists and tree care consumers worldwide.</p>
+									</div>
+									<div class="affiliation eda">
+										<div class="logo"><img src="images/temp/affil-eda.png" alt="EDA" /></div>
+										<h4><a href="">Equipment Dealers Association</a></h4>
+										<p>EDA is committed to building the best business environment for retail agricultural, construction, outdoor power, and large property equipment dealers across the United States and Canada. They achieve this by aiding in the advancement of member dealers, collecting and distributing valuable industry information, fostering cooperation and fair practices among dealer relationships, encouraging equitable practices and high standards in the industry, and more.</p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+			</main>
+		</div>
+
+	</div>
+	<?php include 'footer.php' ?>
+</div>
+</body>
+</html>
